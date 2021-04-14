22e90849eb088ce3aad2a1189209661e8475f792
ehsan <ebaharinasab.eb@gmail.com>
2021-04-15 02:14:26 +0430

    remove useless html file

--- 1/main.js
+++ 1/main.js
@@ -1,8 +1,8 @@
 let arr = ['a', 'b', 'c'];
 
-alert('Befor delete:\n' + arr);     // [ 'a', 'b', 'c' ]
-deleteByIndex(arr, 1);              // remove 'b'
-alert('after delete:\n' + arr);     // [ 'a', 'c' ]
+console.log(arr);       // [ 'a', 'b', 'c' ]
+deleteByIndex(arr, 1);  // remove 'b'
+console.log(arr);       // [ 'a', 'c' ]
 
 function deleteByIndex(array, index) {
     if (index > -1 && index < array.length) {
